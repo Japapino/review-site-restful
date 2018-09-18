10f762a3fe053d6ee1f6d74389ba9501552172a9
Jallippino <35497750+Japapino@users.noreply.github.com>
2018-09-18 11:26:48 -0500

    passed application test

--- src/main/java/com/murakami/reviewsiterestful/Review.java
+++ src/main/java/com/murakami/reviewsiterestful/Review.java
@@ -1,8 +1,10 @@
 package com.murakami.reviewsiterestful;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Review {
 
     @Id
--- src/main/java/com/murakami/reviewsiterestful/ReviewRepository.java
+++ src/main/java/com/murakami/reviewsiterestful/ReviewRepository.java
@@ -8,7 +8,6 @@ import java.util.Set;
 @Repository
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
-    Review findByName(String name);
 
     @Override
     Set<Review> findAll();
